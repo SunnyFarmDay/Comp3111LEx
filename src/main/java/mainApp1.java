8a585e39ac5b6cb2cd581851440e6dc815d7a5c7
SunnyFarmDay <SunnyFarmDay@gmail.com>
2023-09-12 00:42:05 +0800

    This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/mainApp1.java
@@ -0,0 +1,8 @@
+public class mainApp1 {
+public static void main(String[] args) {
+        int b = 2;
+        int n = 11;
+        System.out.println(b+" to power "+n+" is "+myLibrary.Power(b,n));
+        System.out.println(n+"! = "+myLibrary.factorial(n));
+    }
+}
